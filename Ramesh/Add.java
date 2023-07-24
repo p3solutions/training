339ad03a8817c39e2dabf438c5295ab5b03e0d08
RameshP3Solutions <rameshp@platform3solutions.com>
2023-07-24 11:01:12 +0530

    Basics

---
+++ Ramesh/Add.java
@@ -0,0 +1,9 @@
+public class Add {
+	public static void main(String args[]) {
+		int a = 7;
+		int b = 7;
+		
+		System.out.print(a+b);
+		
+	}
+}
---
+++ Ramesh/Main.java
@@ -0,0 +1,5 @@
+public Main{
+	public static void main(string args[]){
+		System.out.println("Ramesh");
+	}
+}
