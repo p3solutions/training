52bf37cc8c71d1c16b566fcd4b05d72c745782db
Sharmi-p3 <SharmilaRaj.S@platform3solutions.com>
2023-07-25 10:27:21 +0530

    second

---
+++ Sharmila Raj S/ArrayImp.java
@@ -0,0 +1,27 @@
+package Prgm;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class ArrayImp {
+	public static void main(String[] args) {
+		
+		ArrayList<String> l = new ArrayList<String>();
+		l.add("Java");
+		l.add("Python");
+		l.add("R");
+		l.add("c#");
+		
+		System.out.println(l);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
